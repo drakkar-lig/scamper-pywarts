drakkar-lig/scamper-pywarts

commit a1da3f592168e8552581d8b660bf4453470a6849

performance: Don't use struct.unpack to extract a single byte
Since a lot of options are only 1-byte long, this gives a substantial
speed-up (around 20%).

warts/parsing.py
@@ -36,7 +36,9 @@ class Parser(object):
         return res
 
     def read_uint8(self):
-        return self.read_from_format('B')[0]
+        res = self.buf[self.offset]
+        self.offset += 1
+        return res
 
     def read_uint16(self):
         return self.read_from_format('>H')[0]

commit 96c1d7a7b7519640dfd7c8aaa8b06f81254437f3

parsing: Fix python2 regression introduced in a1da3f5921

setup.py
@@ -65,6 +65,6 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['six'],
 
 )

warts/parsing.py
@@ -26,6 +26,8 @@ from collections import namedtuple
 import logging
 import socket
 
+import six
+
 from .errors import ParseError, InvalidFormat, EmptyRead, IncompleteRead, ReadError
 
 logger = logging.getLogger(__name__)
@@ -56,7 +58,7 @@ class Parser(object):
         return res
 
     def read_uint8(self):
-        res = self.buf[self.offset]
+        res = six.indexbytes(self.buf, self.offset)
         self.offset += 1
         return res
 